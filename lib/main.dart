e9c3974f234faca6acff8e4dc0c182594e1a3924
moawezz <moawez.de@gmail.com>
2024-08-12 00:43:00 +0330

    localize app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,42 +12,64 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          appBar: AppBar(
-            actions: [
-              const SizedBox(
-                width: 8,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('fa'), // Farsi
+      ],
+      theme: ThemeData(
+        fontFamily: 'Vazirmatn',
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('assets/images/icon.png'),
+            ),
+            const Text('قیمت بروز ارز'),
+            Expanded(
+                child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Image.asset('assets/images/menu.png'))),
+            const SizedBox(
+              width: 10,
+            )
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 16, 16, 8),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Image.asset('assets/images/info_icon.png'),
+                  const SizedBox(width: 8),
+                  const Text('نرخ ارز آزاد چیست؟ '),
+                ],
               ),
-              Expanded(
-                  child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Image.asset('assets/images/menu.png'))),
-              Text('قیمت بروز ارز'),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Image.asset('assets/images/icon.png'),
-              )
+              const Text(
+                ' نرخ ارزها در معاملات نقدی و رایج روزانه است معاملات نقدی معاملاتی هستند که خریدار و فروشنده به محض انجام معامله، ارز و ریال را با هم تبادل می نمایند.',
+                textDirection: TextDirection.rtl,
+              ),
             ],
           ),
-          body: Padding(
-            padding: const EdgeInsets.fromLTRB(16, 16, 16, 8),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text('نرخ ارز آزاد چیست؟ '),
-                    const SizedBox(width: 8),
-                    Image.asset('assets/images/info_icon.png'),
-                  ],
-                ),
-                Text(
-                  ' نرخ ارزها در معاملات نقدی و رایج روزانه است معاملات نقدی معاملاتی هستند که خریدار و فروشنده به محض انجام معامله، ارز و ریال را با هم تبادل می نمایند.',
-                  textDirection: TextDirection.rtl,
-                ),
-              ],
-            ),
-          )),
-    );
+        ));
   }
 }
