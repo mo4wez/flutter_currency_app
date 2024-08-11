2b920c5250cd06a0dfbd09909d900aeaf9a214f1
moawezz <moawez.de@gmail.com>
2024-08-12 02:52:04 +0330

    add ListView builder

--- lib/main.dart
+++ lib/main.dart
@@ -76,60 +76,105 @@ class HomePage extends StatelessWidget {
             )
           ],
         ),
-        body: Padding(
-          padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Image.asset(
-                    'assets/images/info_icon.png',
-                    width: 18,
-                    height: 16,
-                  ),
-                  const SizedBox(width: 8),
-                  Text(
-                    'نرخ ارز آزاد چیست؟ ',
-                    style: Theme.of(context).textTheme.displayLarge,
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              Text(
-                ' نرخ ارزها در معاملات نقدی و رایج روزانه است معاملات نقدی معاملاتی هستند که خریدار و فروشنده به محض انجام معامله، ارز و ریال را با هم تبادل می نمایند.',
-                textDirection: TextDirection.rtl,
-                style: Theme.of(context).textTheme.bodyLarge,
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 24, 0, 0),
-                child: Container(
-                  height: 35,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(1000),
-                      color: Color.fromARGB(255, 130, 130, 130)),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Text(
-                        'نام آزاد ارز',
-                        style: Theme.of(context).textTheme.displayMedium,
-                      ),
-                      Text(
-                        'قیمت',
-                        style: Theme.of(context).textTheme.displayMedium,
-                      ),
-                      Text(
-                        'تغییر',
-                        style: Theme.of(context).textTheme.displayMedium,
-                      ),
-                    ],
+        body: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(28, 16, 28, 16),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Image.asset(
+                      'assets/images/info_icon.png',
+                      width: 18,
+                      height: 16,
+                    ),
+                    const SizedBox(width: 8),
+                    Text(
+                      'نرخ ارز آزاد چیست؟ ',
+                      style: Theme.of(context).textTheme.displayLarge,
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                Text(
+                  ' نرخ ارزها در معاملات نقدی و رایج روزانه است معاملات نقدی معاملاتی هستند که خریدار و فروشنده به محض انجام معامله، ارز و ریال را با هم تبادل می نمایند.',
+                  textDirection: TextDirection.rtl,
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 24, 0, 0),
+                  child: Container(
+                    width: double.infinity,
+                    height: 35,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(1000),
+                        color: Color.fromARGB(255, 130, 130, 130)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(
+                          'نام آزاد ارز',
+                          style: Theme.of(context).textTheme.displayMedium,
+                        ),
+                        Text(
+                          'قیمت',
+                          style: Theme.of(context).textTheme.displayMedium,
+                        ),
+                        Text(
+                          'تغییر',
+                          style: Theme.of(context).textTheme.displayMedium,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+                Container(
+                    width: double.infinity,
+                    height: 700,
+                    child: ListView.builder(
+                        itemCount: 30,
+                        itemBuilder: (BuildContext context, int count) {
+                          return Padding(
+                            padding: const EdgeInsets.fromLTRB(2, 8, 2, 0),
+                            child: Container(
+                              width: double.infinity,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(32),
+                                  boxShadow: <BoxShadow>[
+                                    BoxShadow(
+                                        blurRadius: 1.0, color: Colors.grey),
+                                  ]),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Text(
+                                    'دلار',
+                                    style:
+                                        Theme.of(context).textTheme.bodyLarge,
+                                  ),
+                                  Text(
+                                    '60000',
+                                    style:
+                                        Theme.of(context).textTheme.bodyLarge,
+                                  ),
+                                  Text('+5',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodyLarge),
+                                ],
+                              ),
+                            ),
+                          );
+                        }))
+              ],
+            ),
           ),
         ));
   }
