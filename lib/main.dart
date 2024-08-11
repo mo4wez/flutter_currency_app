2eb3a1accb6f77f169b11217c9b21f6ddfefab02
moawezz <moawez.de@gmail.com>
2024-08-11 23:39:16 +0330

    Implement body: Text and Info icon

--- lib/main.dart
+++ lib/main.dart
@@ -12,26 +12,41 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          actions: [
-            SizedBox(
-              width: 8,
+          appBar: AppBar(
+            actions: [
+              const SizedBox(
+                width: 8,
+              ),
+              Expanded(
+                  child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Image.asset('assets/images/menu.png'))),
+              Text('قیمت بروز ارز'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('assets/images/icon.png'),
+              )
+            ],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.fromLTRB(16, 16, 16, 8),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text('نرخ ارز آزاد چیست؟ '),
+                    const SizedBox(width: 8),
+                    Image.asset('assets/images/info_icon.png'),
+                  ],
+                ),
+                Text(
+                  ' نرخ ارزها در معاملات نقدی و رایج روزانه است معاملات نقدی معاملاتی هستند که خریدار و فروشنده به محض انجام معامله، ارز و ریال را با هم تبادل می نمایند.',
+                  textDirection: TextDirection.rtl,
+                ),
+              ],
             ),
-            Expanded(
-                child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Image.asset('assets/images/menu.png'))),
-            Text('قیمت بروز ارز'),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset('assets/images/icon.png'),
-            )
-          ],
-        ),
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+          )),
     );
   }
 }
